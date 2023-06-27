1fdc787cba0e533b355808dd9b63b3ee8bef6910
alaskannate <64567768+alaskannate@users.noreply.github.com>
2023-06-27 10:51:42 -0700

    inital commit

---
+++ app.js
@@ -0,0 +1,36 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+
+//CONNECT TO MongoDB 
+
+const uri = "mongodb+srv://nateewing93:Travelin.20@cluster0.cxhnnxh.mongodb.net/?retryWrites=true&w=majority";
+
+async function connect() {
+  try {
+    await mongoose.connect(uri);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(error);
+  }
+}
+connect();
+
+//TODO
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
